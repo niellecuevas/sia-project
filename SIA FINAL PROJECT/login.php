e6154ce259fabed33f96d237aed8ed0d25fa4898
Kyle-Ilao <ilaojkyle@gmail.com>
2023-11-02 19:42:05 +0800

    Added Login Functionality and Database Connection

--- SIA FINAL PROJECT/login.php
+++ SIA FINAL PROJECT/login.php
@@ -1,25 +1,36 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<!-- Test -->
+	<!-- Main CSS -->
 	<link rel="stylesheet" href="css/loginform.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<!-- Page Title -->
 	<title>Login Student Management System</title>
 </head>
 <body>
+	<!-- Main Container -->
 	<div class="container" id="container">
+
+		<!-- Main Login Form -->
 		<div class="form-container log-in-container">
 			<form action="#">
+
+				<!-- Login Header -->
 				<img src="img/BSULogo.png" id="BSULogo" alt="logo">
                 <div class="School">
                 <h3 class="red-text"> Welcome Red Spartans!</h3>
-                <h6> Please login your G-suite account</h6>
+                <span> Please login your G-suite account</span>
                 </div>
+
+				<!-- Login Textboxes -->
 				<input type="email" placeholder="Email" />
 				<input type="password" placeholder="Password" />
 				<a href="dashboard.html" data-aos="fade" data-aos-easing="linear" data-aos-duration="1000" data-aos-once="true" class="button">Login</a>
 			</form>
 		</div>
+
+		<!-- Side Panel  -->
 		<div class="overlay-container">
 			<div class="overlay">
 				<div class="overlay-panel overlay-right">
