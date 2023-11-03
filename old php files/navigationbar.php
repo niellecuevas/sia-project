a63cf06183e3d93c39a9f4bb19a3336b61c52c94
niellecuevas <raniella810@gmail.com>
2023-11-04 00:10:01 +0800

    Removed Unnecessary Files

---
+++ old php files/navigationbar.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/navigationbar.css">
+    <link rel="stylesheet" href="css/responsivedashboard.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+	<title>Navigation Bar Student Conduct Management System</title>
+</head>
+<body>
+    <!-- Navigation Bar -->
+    <div class="area"></div><nav class="main-menu">
+    <ul>
+        <li>
+            <a href="dashboard.html">
+                <i class="fa fa-bars fa-2x"></i>
+                <span class="nav-text">
+                   Dashboard
+                </span>
+            </a>
+          
+        </li>
+        <li class="has-subnav">
+            <a href="generatereport.html">
+                <i class="fa fa-file fa-2x"></i>
+                <span class="nav-text">
+                    Generate Report
+                </span>
+            </a>
+            
+        </li>
+        <li class="has-subnav">
+            <a href="managereport.html">
+               <i class="fa fa-clipboard fa-2x"></i>
+                <span class="nav-text">
+                    Manage Report
+                </span>
+            </a>
+            
+        </li>
+        <li class="has-subnav">
+            <a href="reportoverview.html">
+               <i class="fa fa-eye fa-2x"></i>
+                <span class="nav-text">
+                    Report Overview
+                </span>
+            </a>
+           
+        </li>
+        <li>
+            <a href="studentoverview.html">
+                <i class="fa fa-school fa-2x"></i>
+                <span class="nav-text">
+                    Student Overview
+                </span>
+            </a>
+        </li>
+        <li>
+            <a href="accountmanager.html">
+                <i class="fa fa-user fa-2x"></i>
+                <span class="nav-text">
+                   Account Manager
+                </span>
+            </a>
+        </li>
+    </ul>
+
+    <ul class="logout">
+        <li>
+           <a href="logout.html">
+                 <i class="fa fa-power-off fa-2x"></i>
+                <span class="nav-text">
+                    Logout
+                </span>
+            </a>
+        </li>  
+    </ul>
+</nav>
+</body>
+</html>
