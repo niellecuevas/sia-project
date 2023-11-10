5229652909f7233a07e92313652c9571eb617e1a
asheraaguilar <aguilarasherakathryn@gmail.com>
2023-11-10 15:43:18 +0800

    Changes in Student login and Student Homepage

--- SIA FINAL PROJECT/studentHomepage.php
+++ SIA FINAL PROJECT/studentHomepage.php
@@ -9,7 +9,8 @@
         <link rel="icon" href="./img/BSULogo.png" sizes="32x32" type="image/png">
         <!--studhome.css -->
         <link rel="stylesheet" href="./css/studhome.css">
-        <link rel="stylesheet" href="../css/studhome.css">
+        <link rel="stylesheet" href="css/managereport.css">
+        <link rel="stylesheet" href="css/updatepopup.css">
         <!--generate report.css -->
         <link rel="stylesheet" href="../css/generatereport.css">
         <!-- Link Swiper's CSS -->
@@ -29,7 +30,8 @@
         <div class="navbar">
           <div class="logo"><a href=""><img src="./img/BSULogo.png">Batangas State University The NEU</a></div>
           <div class="nav-btns">
-          <a href="callSlip" id="callslip" class="action-btn" >Call Slip</a>
+          <a id="openCallSlipForm" class="action-btn" >Call Slip</a>
+          <a id="openAppealReq" class="action-btn" >Appeal</a>
           <a href="./php/actlogout.php" class="action-btn">Logout</a>
           </div>
           <div class="toggle-btn">
@@ -39,16 +41,68 @@
         
         <div class="dropdown_menu">
           <li><a href="callSlip" id="callslip" class="action-btn" >Call Slip</a></li>
+          <li><a href="appeal" id="appeal" class="action-btn" >Appeal</a></li>
          <li><a href="./php/actlogout.php" class="action-btn">Logout</a></li>
         </div>
       </header>
       <!---------------TOP NAVIGATION END----------------->
+                    
+      <!------------- ANNOUNCEMENT BANNER CAROUSEL ------------------>   
+      <div class="overview">
+            <h1>Student Information</h1>
+      </div> 
+      <div class="slideshow-container">
+
+        <div class="mySlides fade">
+          <img src="./img/studHomeImg/announcementBanner1.png" style="width:100%">
+        </div>
+        
+        <div class="mySlides fade">
+          <img src="./img/studHomeImg/announcementBanner2.png" style="width:100%">
+        </div>
+        
+        <div class="mySlides fade">
+          <img src="./img/studHomeImg/announcementBanner3.png" style="width:100%">
+        </div>
+
+        <div class="mySlides fade">
+          <img src="./img/studHomeImg/announcementBanner4.png" style="width:100%">
+        </div>
+
+        <div class="mySlides fade">
+          <img src="./img/studHomeImg/announcementBanner5.png" style="width:100%">
+        </div>
+
+        <div class="mySlides fade">
+          <img src="./img/studHomeImg/announcementBanner6.png" style="width:100%">
+        </div>
+
+        <div class="mySlides fade">
+          <img src="./img/studHomeImg/announcementBanner7.png" style="width:100%">
+        </div>
+
+        <div class="mySlides fade">
+          <img src="./img/studHomeImg/announcementBanner8.png" style="width:100%">
+        </div>
+
+        <div style="text-align: center" >
+          <span class="dot"></span> 
+          <span class="dot"></span> 
+          <span class="dot"></span>
+          <span class="dot"></span>
+          <span class="dot"></span> 
+          <span class="dot"></span> 
+          <span class="dot"></span> 
+          <span class="dot"></span>   
+        </div>
+        
+        </div>
+
+
+        <!--End-->
 
       <!------------- STUDENT INFORMATON STARTS ------------------> 
         <div class="banner">
-          <div class="overview">
-            <h1>Student Information</h1>
-          </div>
           <div class="studInfo" >
           <div class="nameNsrcode">
           <form>
@@ -56,59 +110,18 @@
           <input type="text" class="info" id="name" name="name" value="<?php echo $_SESSION['FullName']; ?>"><br><br>
           <label for="srcode" id="adjustSrcode">Sr-Code:</label>
           <input type="text" class="info" id="srcode" name="srcode" value="<?php echo $_SESSION['SRCode']; ?>"><br><br>
-      </form>
+          <label for="course">Course:</label>
+          <input type="text" class="info" id="course" name="course" value="<?php echo $_SESSION['CourseName']; ?>"><br><br>
+          <label for="department" id="adjustDept">Department:</label>
+          <input type="text" class="info" id="department" name="department" value="<?php echo $_SESSION['Department']; ?>"><br><br>
+        </form>
       </div>
-          <div class="courseNdept">
-      <form>
-        <label for="course">Course:</label>
-        <input type="text" class="info" id="course" name="course" value="<?php echo $_SESSION['CourseName']; ?>"><br><br>
-        <label for="department" id="adjustDept">Department:</label>
-        <input type="text" class="info" id="department" name="department" value="<?php echo $_SESSION['Department']; ?>"><br><br>
-      </form>
-          </div>
         </div>
         </div>
        </div>
       <!------------- STUDENT INFORMATION END ------------------> 
-                    
-      <!------------- ANNOUNCEMENT BANNER SLIDESHOW ------------------>    
-      <div class="slideshow-container">
 
-        <div class="mySlides fade">
-          <div class="numbertext">1 / 3</div>
-          <img src="./img/studHomeImg/announcementBanner1.png" style="width:100%">
-          
-        </div>
-        
-        <div class="mySlides fade">
-          <div class="numbertext">2 / 3</div>
-          <img src="./img/studHomeImg/announcementBanner2.png" style="width:100%">
-          
-        </div>
-        
-        <div class="mySlides fade">
-          <div class="numbertext">3 / 3</div>
-          <img src="./img/studHomeImg/announcementBanner3.png" style="width:100%">
-          
-        </div>
-        
-        </div>
-        <br>
-        
-        <div style="text-align:center" >
-          <span class="dot"></span> 
-          <span class="dot"></span> 
-          <span class="dot"></span> 
-        </div>
-
-        <!--End-->
-        <!----------- STUDENT CONDUCT INFORMATION START----------------->
-  <div class="openingText">
-        <h1 id="titleVio" data-aos="fade-up">STUDENT CONDUCT INFORMATION</h1>
-        <h3 id="secondTitle" data-aos="fade-up">Student conduct information is use for data and documentation related to the behavior, actions, and disciplinary matters of the students within Batangas State University Lipa Campus.
-        It encompasses a wide range of information that helps institutions monitor and manage student behavior, enforce disciplinary policies, and maintain a safe and conducive learning environment.</h3>
-  </div>
-
+       <!----------- STUDENT CONDUCT INFORMATION START----------------->
 <section>
 <div class="offenses-container" data-aos="fade-right">
     <div class="offense-container">
@@ -127,14 +140,16 @@
       <div class="violationInfo">
           <label for="violation" class="lbl">Violation:</label>
           <input type="text" class="conductInfo" id="violation" name="violation" placeholder="Cross Dressing" readonly><br><br>
+          <label for="violationType" class="lbl">Type:</label>
+          <input type="text" class="conductInfo" id="violationType" name="violationType" placeholder="Minor Offense" readonly><br><br>
           <label for="action" id="adjustSrcode" class="lbl">Action:</label>
           <input type="text" class="conductInfo" id="action" name="action" placeholder="3 days Suspension" readonly><br><br>
           <label for="date" class="lbl">Date:</label>
           <input type="text" class="conductInfo" id="date" name="date" placeholder="10-20-2023" readonly><br><br>
           <label for="time" class="lbl" id="time">Time:</label>
           <input type="text" class="conductInfo" id="time" name="time" placeholder="02:30 PM" readonly><br><br>
-          <label for="remarks" class="lbl" id="remarks">Remarks:</label>
-          <input type="text" class="conductInfo" id="remarks" name="remarks" placeholder="Remarks here"><br><br>
+          <label for="evidence" class="lbl" id="evidence">Evidence:</label>
+          <input type="text" class="conductInfo" id="evidence" name="evidence" placeholder="Evidence here"><br><br>
           <label for="attachment" class="lbl" id="attachment">Attachment:</label>
         <input type="image" src="./img/studHomeImg/violation.jpg" class="conductInfo" alt="violationImage" width="80" height="100">
         </div>
@@ -146,6 +161,8 @@
         <div class="violationInfo">
           <label for="violation" class="lbl">Violation:</label>
           <input type="text" class="conductInfo" id="violation" name="violation" placeholder="Cutting Class" readonly><br><br>
+          <label for="violationType" class="lbl">Type:</label>
+          <input type="text" class="conductInfo" id="violationType" name="violationType" placeholder="Minor Offense" readonly><br><br>
           <label for="action" id="adjustSrcode" class="lbl">Action:</label>
           <input type="text" class="conductInfo" id="action" name="action" placeholder="1 day suspension"readonly><br><br>
           <label for="date" class="lbl">Date:</label>
@@ -154,6 +171,8 @@
           <input type="text" class="conductInfo" id="time" name="time" placeholder="11:00 PM"readonly><br><br>
           <label for="remarks" class="lbl" id="remarks">Remarks:</label>
           <input type="text" class="conductInfo" id="remarks" name="remarks" placeholder="Okay sige"readonly><br><br>
+          <label for="evidence" class="lbl" id="evidence">Evidence:</label>
+          <input type="text" class="conductInfo" id="evidence" name="evidence" placeholder="Evidence here"><br><br>
           <label for="attachment" class="lbl" id="attachment">Attachment:</label>
         <input type="image" src="./img/studHomeImg/violation.jpg" class="conductInfo" alt="violationImage" width="80" height="100">
           </div>
@@ -165,6 +184,8 @@
           <div class="violationInfo">
             <label for="violation" class="lbl">Violation:</label>
             <input type="text" class="conductInfo" id="violation" name="violation" placeholder="Hair Cut" readonly><br><br>
+            <label for="violationType" class="lbl">Type:</label>
+          <input type="text" class="conductInfo" id="violationType" name="violationType" placeholder="Minor Offense" readonly><br><br>
             <label for="action" id="adjustSrcode" class="lbl">Action:</label>
             <input type="text" class="conductInfo" id="action" name="action" placeholder="5 days Suspension" readonly><br><br>
             <label for="date" class="lbl">Date:</label>
@@ -173,6 +194,8 @@
             <input type="text" class="conductInfo" id="time" name="time" placeholder="8:00 PM" readonly><br><br>
             <label for="remarks" class="lbl" id="remarks">Remarks:</label>
             <input type="text" class="conductInfo" id="remarks" name="remarks" placeholder="Kaya mo yan" readonly><br><br>
+            <label for="evidence" class="lbl" id="evidence">Evidence:</label>
+            <input type="text" class="conductInfo" id="evidence" name="evidence" placeholder="Evidence here"><br><br>
             <label for="attachment" class="lbl" id="attachment">Attachment:</label>
           <input type="image" src="./img/studHomeImg/violation.jpg" class="conductInfo" alt="violationImage" width="80" height="100">
             </div>
@@ -184,6 +207,8 @@
             <div class="violationInfo">
               <label for="violation" class="lbl">Violation:</label>
               <input type="text" class="conductInfo" id="violation" name="violation" placeholder="Piercings/Tattoos" readonly><br><br>
+              <label for="violationType" class="lbl">Type:</label>
+          <input type="text" class="conductInfo" id="violationType" name="violationType" placeholder="Minor Offense" readonly><br><br>
               <label for="action" id="adjustSrcode" class="lbl">Action:</label>
               <input type="text" class="conductInfo" id="action" name="action" placeholder="1 day suspension" readonly><br><br>
               <label for="date" class="lbl">Date:</label>
@@ -192,6 +217,8 @@
               <input type="text" class="conductInfo" id="time" name="time" placeholder="09:23 AM" readonly><br><br>
               <label for="remarks" class="lbl" id="remarks">Remarks:</label>
               <input type="text" class="conductInfo" id="remarks" name="remarks" placeholder="Ayaw ko na" readonly><br><br>
+              <label for="evidence" class="lbl" id="evidence">Evidence:</label>
+              <input type="text" class="conductInfo" id="evidence" name="evidence" placeholder="Evidence here"><br><br>
               <label for="attachment" class="lbl" id="attachment">Attachment:</label>
             <input type="image" src="./img/studHomeImg/violation.jpg" class="conductInfo" alt="violationImage" width="80" height="100">
               </div>
@@ -204,24 +231,95 @@
   </div>
 </section>
 
-<!-----------VIOLATION INFORMATION END----------------->
+ <!--callslip-->
+ <section class="containerCallSlipForm">
+        <div class="generate-callslip">
+            <h1>Call Slip</h1>
+        </div>
+        <button class="close-button" onclick="closeCallSlipForm()">&times;</button>
+        <form action="#" class="form">
+        <div class="column">
+          <div class="input-box">
+            <label>SR Code</label>
+            <input type="text" placeholder="Enter SR Code" readonly />
+          </div>
+
+          </div>
+          <div class="input-box">
+            <label>Student</label>
+            <input type="text" placeholder="Enter Student's Full Name" readonly />
+          </div>
+          <div class="input-box">
+            <label>Date</label>
+            <input type="date" id="generateId" placeholder="Enter date" readonly />
+          </div>
+          <div class="column">
+            <div class="input-box">
+              <label>Department</label>
+              <input type="text" placeholder="Enter Department" readonly />
+            </div>
+            <div class="input-box">
+              <label>Program</label>
+              <input type="text" placeholder="Enter program" readonly />
+            </div>
+          </div>
+
+          <div class="column">
+            <div class="input-box">
+              <label>Call Date</label>
+              <input type="date" placeholder="Enter date" readonly />
+            </div>
+            <div class="input-box">
+              <label>Call Time</label>
+              <input type="time" placeholder="Enter time" readonly />
+            </div>
+          </div>
+          <div class="input-box">
+            <label>Action</label>
+            <input type="text" placeholder="Enter action" readonly />      
+          </div>
+          <div class="input-box">
+              <label>Remarks</label>
+              <input type="text" placeholder="Enter remarks" readonly />
+          </div>
+            </div>
+            </div>
+          </div>
+        </form>
+      </section> 
+      
+      <!--appeal request-->
+      <section class="containerAppealRequest">
+        <div class="review-appeal">
+            <h1>Appeal Request</h1>
+        </div>
+        <button class="close-button" onclick="closeAppealRequest()">&times;</button>
+        <form action="#" class="form">
+        <div class="input-box address">
+          <label>Violation</label>
+            <div class="select-box">
+              <select>
+                <option hidden>Violation</option>
+                <option>Option1</option>
+                <option>Option2</option>
+                <option>Option3</option>
+                <option>Option4</option>
+              </select>
+            </div>
+        <div class="input-box">
+          <label>Date</label>
+          <input type="date" id="generateId" placeholder="Enter date" required />
+        </div>
+        <div class="input-box">
+            <label>Request</label>
+            <textarea style="width:100%; height:100px; resize:none;" placeholder="Message here" required ></textarea>
+        </div>
+            <button>Send</button>
+        </form>
+      </section>
 
 <!---------------FOOTER STARTS--------------------->
 <footer>
-  <div class="footer-content">
-      <h3><b>Contact Us</b></h3>
-      <p>If you have other question, you can contact us to the following websites<br>
-        <b>Email :</b> osd.lipa@g.batstate-u.edu.ph<br>
-        <b>Facebook : </b> OSD BatStateULipa<br>
-        <b>Address : </b> A. Tanco Drive, Marawoy, Lipa, Batangas</p>
-      <ul class="socials">
-          <li><a href="https://www.facebook.com/BatStateUTheNEU" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
-          <li><a href="https://twitter.com/BatStateUTheNEU" target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
-          <li><a href="https://batstate-u.edu.ph" target="_blank"><i class="fa-brands fa-square-google-plus"></i></a></li>
-          <li><a href="https://www.youtube.com/@BatStateUTheNEU" target="_blank"><i class="fa-brands fa-youtube"></i></a></li>
-          <li><a href="https://www.linkedin.com/school/batstateutheneu/" target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
-      </ul>
-  </div>
   <div class="footer-bottom">
       <p>copyright &copy;2023 Batangas State University. designed by <span>Group 3</span></p>
   </div>
@@ -229,6 +327,22 @@
 
 <!-----------------FOOTER ENDS--------------------->
 <script src="./js/studHomepage.js"></script>
+<script src="./js/managereporttime.js"></script>
+    <script src="js/managereport.js"></script>
+    <script>
+        document.getElementById("openCallSlipForm").addEventListener("click", function() {
+            document.querySelector(".containerCallSlipForm").style.display = "block";
+        });
+         function closeCallSlipForm() {
+            document.querySelector(".containerCallSlipForm").style.display = "none";
+        }
+        document.getElementById("openAppealReq").addEventListener("click", function() {
+            document.querySelector(".containerAppealRequest").style.display = "block";
+        });
+         function closeAppealRequest() {
+            document.querySelector(".containerAppealRequest").style.display = "none";
+        }
+    </script>
 
 
               
