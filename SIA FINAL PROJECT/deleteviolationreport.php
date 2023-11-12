7eab8f58ee7e5e62ba7a1f09231ef0a0b5c6ad29
Kyle-Ilao <ilaojkyle@gmail.com>
2023-11-12 15:54:57 +0800

    Added Delete Functionality

---
+++ SIA FINAL PROJECT/deleteviolationreport.php
@@ -0,0 +1,41 @@
+<?php
+include('./php/dbconnection.php');
+
+// Check if the request is a POST request
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Check if the 'id' and 'action' parameters are set
+    if (isset($_POST['id']) && isset($_POST['action'])) {
+        // Get the values from the POST request
+        $id = $_POST['id'];
+        $action = $_POST['action'];
+
+        // Check if the action is 'delete'
+        if ($action === 'delete') {
+            // Prepare and execute the delete query
+            $sql = "DELETE FROM tbl_violationreport WHERE ViolationID = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+
+            // Check if the delete was successful
+            if ($stmt->affected_rows > 0) {
+                echo 1; // Success
+            } else {
+                echo 0; // Failure
+            }
+
+            // Close the statement
+            $stmt->close();
+        }
+    } else {
+        // Parameters not set
+        echo "Invalid request";
+    }
+} else {
+    // Not a POST request
+    echo "Invalid request method";
+}
+
+// Close the database connection
+$conn->close();
+?>
