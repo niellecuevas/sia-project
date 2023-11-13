7275625b46271e7bce998015203843b45b57ff3e
niellecuevas <raniella810@gmail.com>
2023-11-13 23:10:51 +0800

    Added Title Icons in Both Logins

--- SIA FINAL PROJECT/loginadmin.php
+++ SIA FINAL PROJECT/loginadmin.php
@@ -4,7 +4,7 @@
     <!-- Main CSS -->
     <link rel="stylesheet" href="css/loginform.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    
+    <link rel="icon" href="./img/BSULogo.png" sizes="32x32" type="image/png">
     <!-- Page Title -->
     <title>Login Student Management System</title>
 </head>
