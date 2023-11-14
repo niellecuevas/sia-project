863fe114d21a6d67efc410fdaf15901640e89f85
Cyrus Tapalla <cyrusthetapalla25@gmail.com>
2023-11-14 20:59:48 +0800

    Fixed Student Appeal

--- SIA FINAL PROJECT/php/studcreateappeal.php
+++ SIA FINAL PROJECT/php/studcreateappeal.php
@@ -4,9 +4,6 @@ require "dbconnection.php";
 
 $violationId = $date = $request = '';
 
-// Set the default value for Status
-$status = 'pending';
-
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the violation ID
@@ -17,11 +14,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $request = $_POST['request'];
 
     // Prepare and execute the stored procedure with the default value for Status
-    $stmt = $conn->prepare("CALL SP_StudentAppeal(?, ?, ?, ?)");
-    $stmt->bind_param("isss", $violationId, $date, $request, $status);
+    $stmt = $conn->prepare("CALL SP_StudentAppeal(?, ?, ?)");
+    $stmt->bind_param("iss", $violationId, $date, $request);
 
     // Check if the stored procedure was executed successfully
     if ($stmt->execute()) {
+        // Check additional success conditions if necessary
+        // For example, if the stored procedure returns a result set, you might want to fetch the results
+        // $result = $stmt->get_result();
+        
         // Redirect to the new student homepage upon successful appeal submission
         header("Location: ../newstudenthomepage.php");
         exit();
